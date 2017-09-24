477b36ab3ee278721db1c40944a79bd3d49f5a90
KiffDatabase <31683772+KiffDatabase@users.noreply.github.com>
2017-09-24 17:34:59 +0300

    Initial commit

---
+++ src/com/company/WeatherApp.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class WeatherApp {
+
+    
+    public boolean ifHighestTempIsHigherThanLowest(int highTemp, int lowTemp) {
+        return true;
+    }
+
+
+    public static int GetCoordinates(String coordinate) {
+
+        return 0;
+    }
+
+    public static int GetCurrentTemperature(int temperature, String city) {
+        return 0;
+    }
+
+    public boolean IfDataUpdated(String lastUpdateTime) {
+        return true;
+    }
+}
---
+++ src/com/company/WeatherAppTest.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+
+class WeatherAppTest {
+
+    WeatherApp weatherapp = new WeatherApp();
+
+    @Test
+    public void testGetCoordinates() throws Exception {
+    }
+    @Test
+    public void testIfValidCoordinateFormat() throws Exception{
+    }
+    @Test
+    public void testGetCurrentTemperature() throws Exception {
+    }
+    @Test
+    public void testIfValidTemperatureFormat() throws Exception{
+    }
+    @Test
+    public void testGetFutureTemperatures() throws Exception{
+    }
+    @Test
+    public void testIshighestTemperature() throws Exception{
+    }
+    @Test
+    public void testIsLowestTemperature() throws Exception{
+    }
+    @Test
+    public void testIfHighTemperatureIsHigherThanLowest() throws Exception {
+    }
+    @Test
+    public void testSplitFutureTemperaturesIntoThree() throws Exception{
+    }
+    @Test
+    public void testIfDataUpdated() throws Exception{
+    }
+    @Test
+    public void testIfPreviousCityLocationMatchesCurrent() throws Exception{
+    }
+    @Test
+    public void testIfCiyLocationIsUnique() throws Exception{
+    }
+    @Test
+    public void testIfSomeFieldIsEmpty() throws Exception{
+    }
+}
